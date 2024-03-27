339c31e4bb395e92e8ced3080f918478456598c7
Mrutunjay113 <mrutunjay765@gmail.com>
2024-03-27 13:28:54 +0530

    add o(n)

--- big-0/big-0.js
+++ big-0/big-0.js
@@ -13,7 +13,8 @@
 */
 
 //Time complexity of the function
-const nemo = ["nemo"];
+
+/*const nemo = ["nemo"];
 const everyone = [
   "dory",
   "bruce",
@@ -40,7 +41,21 @@ function findNemo(array) {
   let t1 = performance.now();
   console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds");
 }
-/* findNemo(large); //18.565 milliseconds
+ findNemo(large); //18.565 milliseconds
 findNemo(everyone); //4.556 milliseconds
 findNemo(nemo); //4.77 milliseconds
 */
+
+//BIG O NOTATION
+//O(n) --> Linear Time
+
+//O(1) --> Constant Time
+//O(1) is the best time complexity
+//O(n) is the worst time complexity
+
+const boxes = [0, 1, 2, 3, 4, 5];
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); //O(1)
+  console.log(boxes[1]); //O(1)
+}
+logFirstTwoBoxes(boxes); //O(2)
